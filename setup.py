dcc3119921d116f48f24f0ceedee5f6d7b61b8e7
Turbidsoul <sccn.sq@gmail.com>
2014-06-18 17:51:59 +0800

    update package struct

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@
 from distutils.core import setup
 
 
-setup(name='tsutil', version='0.1.0', py_modules=['enum', 'encoder', 'util'],
+setup(name='tsutil', version='0.1.3', py_modules=['tsutil.enum', 'tsutil.encoder', 'tsutil.util'],
       author='Turbidsoul Chen', author_email='sccn.sq+py@gmail.com',
-      url='http://blog.turbidsoul.me', description='my python tool module')
+      url='http://blog.turbidsoul.me', description='my python tool module', listense='MIT')
