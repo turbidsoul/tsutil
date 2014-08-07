35fe01413e1e8868776704c8b3492877f3df63e6
Turbidsoul <sccn.sq@gmail.com>
2014-08-07 17:52:24 +0800

    add head sord

--- tsutil/sorted.py
+++ tsutil/sorted.py
@@ -3,7 +3,7 @@
 # @Author: Turbidsoul Chen
 # @Date:   2014-07-30 10:07:05
 # @Last Modified by:   Turbidsoul Chen
-# @Last Modified time: 2014-07-30 10:14:44
+# @Last Modified time: 2014-08-07 17:51:57
 
 
 
@@ -25,3 +25,28 @@ def quick_sorted(lst, func=lambda a, b: cmp(a, b), reversed=False):
         return (before + [pivot] + after)[::-1]
     else:
         return before + [pivot] + after
+
+
+def head_sort(lst, func=lambda a, b: cmp(a, b), reversed=False):
+    def make_maxhead(lst, start, end):
+        root = start
+        while 1:
+            left = root * 2 + 1
+            right = root * 2 + 2
+            if left > end:
+                break
+            child = left
+            if right <= end and func(lst[left], lst[right]) < 0:
+                child = right
+            if func(lst[root], lst[child]) < 0:
+                lst[root], lst[child] = lst[child],lst[root]
+                root = child
+            else:
+                break
+    n = len(lst)
+    for i in range(n, -1, -1):
+        make_maxhead(lst, i, n-1)
+    for end in range(n-1, -1, -1):
+        lst[0], lst[end] = lst[end], lst[0]
+        make_maxhead(lst, 0, end-1)
+    return lst if reversed else lst[::-1]
