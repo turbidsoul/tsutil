1ea8f90be721b34e9b3d8fb78e317e77792bc67c
Turbidsoul <sccn.sq@gmail.com>
2020-06-13 09:14:23 +0800

    更新python3.8

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ if not platform.python_version()[:-2] < '2.7':
 
 setup(
       name='tsutil',
-      version='0.1.6.6',
-      py_modules=['tsutil.enum', 'tsutil.encoder', 'tsutil.util', 'tsutil.daemonize', 'tsutil.sorted', 'tsutil.sync'],
+      version='0.1.7.0',
+      py_modules=['tsutil.enum', 'tsutil.encoder', 'tsutil.util', 'tsutil.daemonize', 'tsutil.sorted', 'tsutil.sync', 'tsutil.sequence'],
       author='Turbidsoul Chen',
       author_email='sccn.sq+py@gmail.com',
       url='http://github.com/turbidsoul/tsutil',
